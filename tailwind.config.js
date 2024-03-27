87c5ff554832c060cf42374705bbcd64c57ec473
Bart Erkamp <bart.erkamp@maxserv.com>
2024-03-27 16:04:27 +0100

    feat: Extend Tailwind config with custom colors
    This commit extends the Tailwind CSS configuration file to include custom colors as per the design specifications.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
             fontFamily: {
                 rubik: ["Rubik", "sans-serif"],
             },
+            colors: {
+                purple: "#A729F5",
+                squidInk: "#313E51",
+                midnightHaze: "#3B4D66",
+                payneGray: "#626C7F",
+                steelBlue: "#ABC1E1",
+                antiFlashWhite: "#F4F6FA",
+                ufoGreen: "#26D782",
+                grapefruitRed: "#EE5454",
+            },
         },
     },
     plugins: [],
