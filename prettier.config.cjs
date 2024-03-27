26326c28547ea6ce2f60b47cdca6046452185cc7
Bart Erkamp <bart.erkamp@maxserv.com>
2024-03-27 15:11:55 +0100

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    endOfLine: "lf",
+    semi: false,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
+    plugins: ['prettier-plugin-tailwindcss']
+}
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <h1 className="font-rubik text-3xl italic">Hello world!</h1>
+            <h1 className="font-rubik text-3xl">Hello world!</h1>
+            <h1 className="font-rubik text-3xl font-medium">Hello world!</h1>
+        </>
+    )
+}
+export default App
